38f9935d6850c6a89adf7e3c461849193492db7a
kangsudal <kangsudal@gmail.com>
2024-12-16 22:22:06 +0900

    HomeView 인디케이터 통합

--- lib/view/home_view/home_view.dart
+++ lib/view/home_view/home_view.dart
@@ -3,16 +3,25 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:literacyk/config/route_names.dart';
+import 'package:literacyk/constants/firebase_constants.dart';
 import 'package:literacyk/view/board_view/board_view_in_home_view.dart';
+import 'package:literacyk/view/board_view/board_view_model.dart';
 import 'package:literacyk/view/user_profile_view/user_profile_view_in_home_view.dart';
+import 'package:literacyk/view/user_profile_view/user_profile_viewmodel.dart';
 
 class HomeView extends ConsumerWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final uid = fbAuth.currentUser!.uid;
+    final userProfileState = ref.watch(userProfileViewmodelProvider(uid));
+    final boardState = ref.watch(boardViewModelProvider);
+
+    final isLoading = userProfileState.isLoading || boardState.isLoading;
+
     return Scaffold(
-      body: Padding(
+      body: isLoading?Center(child: CircularProgressIndicator()):Padding(
         padding: EdgeInsets.only(top: MediaQuery.paddingOf(context).top),
         child: Column(
           children: [
